f4938397f334780db25592a5911b55e946d5c50b
Emil johan Endresen <aom022025@gmail.com>
2025-02-24 10:13:04 +0100

    intial.commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("hello world")
+
+// var x= 5// vi bruker ikke var lengre
+
+const name = "emil"
+
+let firstname = "emil"// string
+let age = "30"  //integer/number
+let examplefloa = 30.2 // float
+let frombergen = true // boolean
+let exampleArray = ["mazda" ,"opel","saab","mitsubishi"]// kan ha nummer variable med flere items
+
+let myObject = {
+  firstname: "torleif",
+  lastname:"leiknes",
+  age: 67,
+  email: "torleif@leiknes.no"
+}
+console.log(myObject)
+console.log(exampleArray)
+
+//console.log(examplefloat)
+let x = 11
+let y = 4
+
+x++
+console.log(x)
+
+
+let z = x + y
+console.log(z)
